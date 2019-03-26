c9d1ec0d15bb541b6881b56b5707f99c6e03f889
Klaus Reuter <khr@mpcdf.mpg.de>
2019-03-26 12:33:39 +0100

    improve test_fileio_logw.py and test_fileio_forces.py to use temporary files

--- test/optimize/test_fileio_forces.py
+++ test/optimize/test_fileio_forces.py
@@ -1,12 +1,14 @@
+"""Convert pickle input files for the forces tests into HDF5,
+read them back in and compare.  Use temporary HDF5 files.
+"""
 
 import os
-import sys
 import numpy as np
 import pickle
 from bioen import optimize
-#from bioen import fileio_modif as fio
 from bioen import fileio as fio
 import h5py
+import tempfile
 
 tol = 5.e-14
 tol_grad = 5.e-12
@@ -18,21 +20,23 @@ filenames_forces = [
 
 
 def test_fileio_forces():
-
     for filename_pkl in filenames_forces:
+        # filename_hdf5 = os.path.splitext(filename_pkl)[0] + ".h5"
+        filename_hdf5 = tempfile.NamedTemporaryFile(mode='w', suffix=".h5", delete=False)
+        filename_hdf5.close()
 
-        filename_hdf5 = os.path.splitext(filename_pkl)[0] + ".h5"
-        print (filename_pkl, filename_hdf5)
+        print (filename_pkl, filename_hdf5.name)
 
         mylist = ["forces_init", "w0", "y", "yTilde", "YTilde", "theta"]
-        fio.convert_to_hdf5(filename_pkl, filename_hdf5, mylist)
+        fio.convert_to_hdf5(filename_pkl, filename_hdf5.name, mylist)
 
         # load pickle
         with open(filename_pkl, 'rb') as ifile:
             x = pickle.load(ifile)
 
         # load hdf5
-        mydict = fio.load(filename_hdf5)
+        mydict = fio.load(filename_hdf5.name)
+        os.unlink(filename_hdf5.name)
 
         # compare
         for i in range(len(mylist)):
--- test/optimize/test_fileio_logw.py
+++ test/optimize/test_fileio_logw.py
@@ -1,12 +1,14 @@
+"""Convert pickle input files for the logw tests into hdf5,
+read them back in and compare.
+"""
 
 import os
-import sys
 import numpy as np
 import pickle
 from bioen import optimize
-#from bioen import fileio_modif as fio
 from bioen import fileio as fio
 import h5py
+import tempfile
 
 tol = 5.e-14
 tol_grad = 5.e-14
@@ -24,28 +26,28 @@ filenames_logw = [
 
 
 def test_fileio_logw():
-
     for filename_pkl in filenames_logw:
-
-        filename_hdf5 = os.path.splitext(filename_pkl)[0] + ".h5"
-        print (filename_pkl, filename_hdf5)
+        # filename_hdf5 = os.path.splitext(filename_pkl)[0] + ".h5"
+        filename_hdf5 = tempfile.NamedTemporaryFile(mode='w', suffix=".h5", delete=False)
+        filename_hdf5.close()
+        print (filename_pkl, filename_hdf5.name)
 
         mylist = ["GInit", "G", "y", "yTilde", "YTilde", "w0", "theta"]
-        fio.convert_to_hdf5(filename_pkl, filename_hdf5, mylist)
+        fio.convert_to_hdf5(filename_pkl, filename_hdf5.name, mylist)
 
         # load pickle
         with open(filename_pkl, 'rb') as ifile:
             x = pickle.load(ifile)
 
         # load hdf5
-        mydict = fio.load(filename_hdf5)
+        mydict = fio.load(filename_hdf5.name)
+        os.unlink(filename_hdf5.name)
 
         # compare
         for i in range(len(mylist)):
             if (np.isscalar(x[i])):
                 df = optimize.util.compute_relative_difference_for_values(
                     x[i], mydict[mylist[i]])
-                #print("\trelative difference of scalar  = {}  --> values {} .. {}".format(df,x[i], mydict[mylist[i]]))
                 print("\t ({}) [{:14s}]\t - relative difference of scalar = {}  --> values {} .. {}".format(
                     i, mylist[i], df, x[i], mydict[mylist[i]]))
                 assert(df < tol)
@@ -54,5 +56,4 @@ def test_fileio_logw():
                     x[i], mydict[mylist[i]])
                 print(
                     "\t ({}) [{:14s}]\t - relative difference of vector = {} (maximum at index {})".format(i, mylist[i], dg, idx))
-                #print("\trelative difference of vector = {} (maximum at index {})".format(dg, idx))
                 assert(dg < tol_grad)
