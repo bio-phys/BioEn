1616084651eed74bec5632ab3b7c2512d5934102
Klaus Reuter <khr@mpcdf.mpg.de>
2022-07-04 09:00:25 +0200

    try local upload to pypi

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -44,3 +44,9 @@ for PY in $CPYTHONS; do
     cd -
 done
 
+# Upload packages to local registry
+pip install twine
+TWINE_PASSWORD=${CI_JOB_TOKEN} TWINE_USERNAME=gitlab-ci-token \
+  ${PYBIN}/python -m \
+    twine upload --repository-url ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/pypi wheelhouse/*
+
