ca4f89aaa76a359017c0d1e33f82b33f8b928137
Klaus Reuter <khr@mpcdf.mpg.de>
2022-07-19 17:08:54 +0200

    fix numpy install for sdist build

--- scripts/build_sdist.sh
+++ scripts/build_sdist.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-PYTHON=${PYTHON:-/opt/python/cp39-cp39/bin/python3}
+set -e -x
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-$PYTHON setup.py sdist --formats=gztar
+
+PYBIN=${PYBIN:-/opt/python/cp39-cp39/bin}
+
+${PYBIN}/pip3 install --no-input numpy
+
+${PYBIN}/python3 setup.py sdist --formats=gztar
+
+${PYBIN}/pip3 uninstall --yes numpy
 
